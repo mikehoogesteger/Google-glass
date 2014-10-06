a6f8afb04009cf20e32993360aaa82ef28ec5b8f
unknown <MikeMo@MikeMo01.corp.infosupport.com>
2014-10-06 15:52:59 +0200

    working project

---
+++ gdk-charades-sample-master/gen/com/google/android/glass/sample/charades/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.android.glass.sample.charades;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
