a66d4564a8d9cab10bf556138d9eec0ed611e65c
MikeMolIS <mike.mol@infosupport.com>
2014-10-13 07:24:51 +0200

    add icon and immersion main

---
+++ Scanner/src/com/infosupport/MainActivity.java
@@ -0,0 +1,65 @@
+package com.infosupport;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.glass.widget.CardBuilder;
+
+public class MainActivity extends Activity {
+	private static final String TAG = "MainActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		View view = new CardBuilder(this, CardBuilder.Layout.TEXT)
+	    .setText("Kenteken scanner")
+	    .getView();
+		
+		setContentView(view);
+	}
+
+	@Override
+	public boolean onKeyDown(int keycode, KeyEvent event) {
+		Log.i(TAG, "onKeyDown");
+		if (keycode == KeyEvent.KEYCODE_DPAD_CENTER) {
+			openOptionsMenu();
+			return true;
+		} else if (keycode == KeyEvent.KEYCODE_BACK) {
+			finish();
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.scan:
+			Log.i(TAG, "Menu item scan");
+			Intent intent = new Intent(this, OCRActivity.class);
+			startActivity(intent);
+			return true;
+
+		case R.id.stop:
+			finish();
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+}
