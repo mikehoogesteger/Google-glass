f03d133d0c1a5df0371af025c298c41ef0ad3568
Mike Hoogesteger <mike.hoogesteger@infosupport.com>
2014-11-12 11:54:12 +0100

    Output of multiple kentekens

--- Scanner/src/com/infosupport/ResultActivity.java
+++ Scanner/src/com/infosupport/ResultActivity.java
@@ -27,11 +27,12 @@ import android.widget.TextView;
 public class ResultActivity extends Activity implements TaskDelegate {
 	
 	private static final String TAG = "ResultActivity";
+	private static final int CENTER = 0;
+	private static final int START = 1;
+	private static final int END = 2;
+	private static final String VERZEKERD = "Verzekerd";
+	private static final String ONVERZEKERD = "Onverzekerd";
 	
-	private TextView mKenteken;
-	private TextView mVerzekerd;
-	private TextView mAPK;
-	private TextView mError;
 	private ArrayList<String> kentekens;
 	private KentekenServiceCaller sc;
 
@@ -113,49 +114,7 @@ public class ResultActivity extends Activity implements TaskDelegate {
 	 */
 	@Override
 	public void taskCompletionResult(JSONObject result) {
-		setContentView(R.layout.result);
-		if (result == null) {
-			mVerzekerd = (TextView) findViewById(R.id.verzekerd_center);
-			mVerzekerd.setText("Het kenteken komt niet voor in de database");
-		} else {
-			try {
-				String kenteken = result.getJSONObject("resource").getString("Kenteken");
-				String verzekerd = result.getJSONObject("resource").getString(
-						"WAMverzekerdgeregistreerd");
-				String apk = result.getJSONObject("resource").getString(
-						"VervaldatumAPK");
-				Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss",
-						Locale.US).parse(apk);
-				String formattedApk = new SimpleDateFormat("dd/MM/yyyy",
-						Locale.US).format(date);
-
-				mKenteken = (TextView) findViewById(R.id.kenteken_center);
-				mVerzekerd = (TextView) findViewById(R.id.verzekerd_center);
-				mAPK = (TextView) findViewById(R.id.apk_center);
-
-				mKenteken.setText("Kenteken: " + kenteken);
-				if (verzekerd == "true") {
-					mVerzekerd.setText("Verzekerd: Ja");
-				} else {
-					mVerzekerd.setText("Verzekerd: Nee");
-				}
-				mAPK.setText("APK verloopt op: " + formattedApk);
-			} catch (JSONException | ParseException e) {
-				try {
-					String error = result.getJSONObject("error").getString(
-							"message");
-
-//					mError = (TextView) findViewById(R.id.error);
-//					mError.setText(error);
-				} catch (JSONException e1) {
-					Log.e(TAG, "catch -> error niet gevonden");
-					e1.printStackTrace();
-				}
-				Log.e(TAG,
-						"catch -> resource niet gevonden | datum kan niet geparst worden");
-				e.printStackTrace();
-			}
-		}
+		
 	}
 
 	@Override
@@ -163,68 +122,103 @@ public class ResultActivity extends Activity implements TaskDelegate {
 		Log.i(TAG, kentekens.toString());
 		setContentView(R.layout.result);
 		
-		if (result.size() != 0) {
-			
-			mKenteken = (TextView) findViewById(R.id.kenteken_center);
-			mVerzekerd = (TextView) findViewById(R.id.verzekerd_center);
-			mAPK = (TextView) findViewById(R.id.apk_center);
-			
-			String kenteken = "";
-			String verzekerd = "";
-			String apk = "";
-			
-			//for (JSONObject json : result) {
-				/*JSONObject json = result.get(0);
-				try {
-					kenteken = json.getJSONObject("resource").getString("Kenteken") + " ";
-					verzekerd = json.getJSONObject("resource").getString(
-							"WAMverzekerdgeregistreerd") != null && json.getJSONObject("resource").getString(
-									"WAMverzekerdgeregistreerd").equals("true") ? "Verzekerd " : "Onverzekerd ";
-					String tempapk = json.getJSONObject("resource").getString(
-							"VervaldatumAPK");
-					Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss",
-							Locale.US).parse(tempapk);
-					apk = new SimpleDateFormat("dd/MM/yyyy",
-							Locale.US).format(date) + " ";
-					
-				} catch (JSONException | ParseException e) {
-					kenteken = "";
-					verzekerd = "Er konden geen gegevens gevonden worden bij het kenteken: " + kentekens.get(0);
-					apk = "";
-				}*/
-			//}
-			/*mKenteken.setText(kenteken);
-			mVerzekerd.setText(verzekerd);
-			mAPK.setText(apk);*/
-			System.out.println("Tekst wordt gewijzigd");
-			mKenteken.setText("99WK07");
-			mVerzekerd.setText("Onverzekerd");
-			mAPK.setText("05/12/2014");
+		int size = result.size();
+		
+		if (size != 0) {
+			setText(result.get(0), kentekens.get(0), CENTER);
+			if (size > 1) {
+				setText(result.get(1), kentekens.get(1), START);
+			}
+			if (size > 2) {
+				setText(result.get(2), kentekens.get(2), END);
+			}
 		} else {
-			mVerzekerd = (TextView) findViewById(R.id.verzekerd_center);
+			TextView mVerzekerd = (TextView) findViewById(R.id.verzekerd_center);
 			mVerzekerd.setText("Er zijn geen kentekens gevonden");
 		}
-		setLeftText();
-		setRightText();
 	}
 	
-	public void setLeftText() {
-		mKenteken = (TextView) findViewById(R.id.kenteken_left);
-		mVerzekerd = (TextView) findViewById(R.id.verzekerd_left);
-		mAPK = (TextView) findViewById(R.id.apk_left);
+	private void setText(JSONObject json, String kenteken, int place) {
+		String verzekerd;
+		String apk;
+		boolean valid = false;
+		try {
+			verzekerd = json.getJSONObject("resource").getString(
+					"WAMverzekerdgeregistreerd") != null && json.getJSONObject("resource").getString(
+							"WAMverzekerdgeregistreerd").equals("true") ? VERZEKERD : ONVERZEKERD;
+			String tempapk = json.getJSONObject("resource").getString(
+					"VervaldatumAPK");
+			Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss",
+					Locale.US).parse(tempapk);
+			if (date.compareTo(new Date()) > 0 && verzekerd.equals(VERZEKERD)) {
+				valid = true;
+			}
+			
+			apk = new SimpleDateFormat("dd/MM/yyyy",
+					Locale.US).format(date);
+			
+		} catch (JSONException | ParseException e) {
+			valid = false;
+			verzekerd = "Geen info\ngevonden";
+			apk = "";
+		}
 		
-		mKenteken.setText("94TNDL");
-		mVerzekerd.setText("Verzekerd");
-		mAPK.setText("23/07/2015");
+		switch (place) {
+			case CENTER:
+				setCenterText(kenteken, verzekerd, apk, valid);
+				break;
+			case START:
+				setLeftText(kenteken, verzekerd, apk, valid);
+				break;
+			case END:
+				setRightText(kenteken, verzekerd, apk, valid);
+				break;
+		}
+		
 	}
 	
-	public void setRightText() {
-		mKenteken = (TextView) findViewById(R.id.kenteken_right);
-		mVerzekerd = (TextView) findViewById(R.id.verzekerd_right);
-		mAPK = (TextView) findViewById(R.id.apk_right);
+	private void setLeftText(String kenteken, String verzekerd, String apk, boolean valid) {
+		setColor(R.id.left_content, valid);
 		
-		mKenteken.setText("01SK99");
-		mVerzekerd.setText("Onverzekerd");
-		mAPK.setText("21/11/2020");
+		TextView mKenteken = (TextView) findViewById(R.id.kenteken_left);
+		TextView mVerzekerd = (TextView) findViewById(R.id.verzekerd_left);
+		TextView mAPK = (TextView) findViewById(R.id.apk_left);
+		
+		mKenteken.setText(kenteken);
+		mVerzekerd.setText(verzekerd);
+		mAPK.setText(apk);
+	}
+	
+	private void setCenterText(String kenteken, String verzekerd, String apk, boolean valid) {
+		setColor(R.id.center_content, valid);	
+		
+		TextView mKenteken = (TextView) findViewById(R.id.kenteken_center);
+		TextView mVerzekerd = (TextView) findViewById(R.id.verzekerd_center);
+		TextView mAPK = (TextView) findViewById(R.id.apk_center);
+		
+		mKenteken.setText(kenteken);
+		mVerzekerd.setText(verzekerd);
+		mAPK.setText(apk);
+	}
+	
+	
+	private void setRightText(String kenteken, String verzekerd, String apk, boolean valid) {
+		setColor(R.id.right_content, valid);
+		
+		TextView mKenteken = (TextView) findViewById(R.id.kenteken_right);
+		TextView mVerzekerd = (TextView) findViewById(R.id.verzekerd_right);
+		TextView mAPK = (TextView) findViewById(R.id.apk_right);
+		
+		mKenteken.setText(kenteken);
+		mVerzekerd.setText(verzekerd);
+		mAPK.setText(apk);
+	}
+	
+	private void setColor(int id, boolean valid) {
+		if (valid) {
+			findViewById(id).setBackgroundResource(R.drawable.green_border);
+		} else {
+			findViewById(id).setBackgroundResource(R.drawable.red_border);
+		}
 	}
 }
